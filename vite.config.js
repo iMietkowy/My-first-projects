4931a7dab38e6b364030e8ed833d2e0437970fd3
iMietkowy <wielgosz.kamill@gmail.com>
2025-03-09 19:56:24 +0100

    add_vite_

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+/**
+ * Path to exercise folder
+ */
+const exercisePath = "BestShop";
+
+/**
+ * Don't change those lines below
+ */
+export default defineConfig({
+	root: exercisePath,
+	server: {
+		port: 3000,
+		open:true,
+	},
+});
